311183055ed3a35524b43625e8b4773df252bce1
louis <liu.louis91@gmail.com>
2024-09-10 16:46:07 -0700

    basic drive code runs

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,8 +1,9 @@
+// PORT 9 IS BROKEN 
 // ports
-const int LEFT_BACK_PORT = 10;
+const int LEFT_BACK_PORT = -10;
 const int LEFT_FRONT_UPPER_PORT = 8;
-const int LEFT_FRONT_LOWER_PORT = 9;
+const int LEFT_FRONT_LOWER_PORT = -6;
 const int RIGHT_BACK_PORT = 19;
-const int RIGHT_FRONT_UPPER_PORT = 18;
+const int RIGHT_FRONT_UPPER_PORT = -18;
 const int RIGHT_FRONT_LOWER_PORT = 17;
 const int IMU_PORT = 7;
